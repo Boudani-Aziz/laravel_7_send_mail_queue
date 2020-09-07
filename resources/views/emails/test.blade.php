d16a75d78033f7877a224607e61fd12e4d6e058b
Aziz Boudani <boudani@gmail.com>
2020-09-07 13:20:26 +0200

    first commit

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>How to send mail using queue in Laravel 7?</title>
+</head>
+<body>
+   
+<center>
+<h2 >
+    How to send mail using queue in Laravel 7?
+</h2>
+</center>
+  
+<p>Hallo zusammen </p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. </p>
+  
+<strong>Danke dir </strong>
+  
+</body>
+</html>
